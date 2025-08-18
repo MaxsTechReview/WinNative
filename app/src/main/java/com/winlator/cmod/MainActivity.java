78a739d2b4e36a6fa7bb501499868b7ee97c0f62
pipetto-crypto <pipetto-crypto@gmail.com>
2025-08-18 18:13:52 +0200

    Remove everything related to SaveManager

--- app/src/main/java/com/winlator/cmod/MainActivity.java
+++ app/src/main/java/com/winlator/cmod/MainActivity.java
@@ -37,13 +37,9 @@ import androidx.preference.PreferenceManager;
 import com.google.android.material.navigation.NavigationView;
 import com.winlator.cmod.R;
 import com.winlator.cmod.contentdialog.ContentDialog;
-import com.winlator.cmod.contentdialog.SaveEditDialog;
-import com.winlator.cmod.contentdialog.SaveSettingsDialog;
 import com.winlator.cmod.core.Callback;
 import com.winlator.cmod.core.PreloaderDialog;
 import com.winlator.cmod.container.ContainerManager;
-import com.winlator.cmod.saves.Save;
-import com.winlator.cmod.saves.SaveManager;
 import com.winlator.cmod.xenvironment.ImageFsInstaller;
 
 import java.util.List;
@@ -60,14 +56,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private int selectedProfileId;
     private Callback<Uri> openFileCallback;
     private SharedPreferences sharedPreferences;
-
-    // Add SaveSettingsDialog and SaveEditDialog instances
-    private SaveSettingsDialog saveSettingsDialog;
-    private SaveEditDialog saveEditDialog;
-    private SaveManager saveManager;
     private ContainerManager containerManager;
-
-    private SaveEditDialog currentSaveEditDialog;
 
     private boolean isDarkMode;
 
@@ -162,8 +151,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setNavigationViewItemTextColor(navigationView, textColor);
         
 
-        // Initialize SaveManager and ContainerManager
-        saveManager = new SaveManager(this);
         containerManager = new ContainerManager(this);
 
         Intent intent = getIntent();
@@ -216,51 +203,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        Log.d("WinActivity", "onActivityResult called with requestCode: " + requestCode + " and resultCode: " + resultCode);
-
-        if (saveSettingsDialog != null && saveSettingsDialog.isShowing()) {
-            Log.d("WinActivity", "Forwarding result to SaveSettingsDialog");
-            saveSettingsDialog.onActivityResult(requestCode, resultCode, data);
-        } else if (saveEditDialog != null && saveEditDialog.isShowing()) {
-            Log.d("WinActivity", "Forwarding result to SaveEditDialog");
-            saveEditDialog.onActivityResult(requestCode, resultCode, data);
-        } else {
-            Log.d("WinActivity", "No dialog found for request code: " + requestCode);
-        }
-    }
-
-    private void showSavesFragment() {
-        SavesFragment fragment = new SavesFragment();
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.FLFragmentContainer, fragment)
-                .commit();
-    }
-
-    // Method to show SaveEditDialog
-    public void showSaveEditDialog(Save saveToEdit) {
-        saveEditDialog = new SaveEditDialog(this, saveManager, containerManager, saveToEdit);
-
-        // Check for dark mode and set the background accordingly
-        if (isDarkMode) {
-            saveEditDialog.getWindow().setBackgroundDrawableResource(R.drawable.content_dialog_background_dark);
-        } else {
-            saveEditDialog.getWindow().setBackgroundDrawableResource(R.drawable.content_dialog_background);
-        }
-
-        saveEditDialog.show();
-    }
-
-    public void onSaveAdded() {
-        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.FLFragmentContainer);
-        if (currentFragment instanceof SavesFragment) {
-            ((SavesFragment) currentFragment).refreshSavesList();
-        }
-    }
-
-    @Override
     public void onBackPressed() {
         FragmentManager fragmentManager = getSupportFragmentManager();
         List<Fragment> fragments = fragmentManager.getFragments();
@@ -305,32 +247,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 drawerLayout.openDrawer(GravityCompat.START);
             }
             return true;
-        } else if (menuItem.getItemId() == R.id.saves_menu_add) {
-            // Check if we are editing a save
-            Intent intent = getIntent();
-            int editSaveId = intent.getIntExtra("edit_save_id", -1);
-            Save saveToEdit = editSaveId >= 0 ? saveManager.getSaveById(editSaveId) : null;
-
-            // Create and show SaveEditDialog or SaveSettingsDialog as appropriate
-            if (saveToEdit != null) {
-                // Ensure previous dialog is dismissed before showing a new one
-                if (saveEditDialog != null && saveEditDialog.isShowing()) {
-                    saveEditDialog.dismiss();
-                }
-                showSaveEditDialog(saveToEdit); // Use the correct method to show SaveEditDialog
-            } else {
-                saveSettingsDialog = new SaveSettingsDialog(this, saveManager, containerManager);
-
-                // Check for dark mode and set the background accordingly
-                if (isDarkMode) {
-                    saveSettingsDialog.getWindow().setBackgroundDrawableResource(R.drawable.content_dialog_background_dark);
-                } else {
-                    saveSettingsDialog.getWindow().setBackgroundDrawableResource(R.drawable.content_dialog_background);
-                }
-
-                saveSettingsDialog.show();
-            }
-            return true;
         } else {
             return super.onOptionsItemSelected(menuItem);
         }
@@ -366,9 +282,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
             case R.id.main_menu_adrenotools_gpu_drivers:
                 show(new AdrenotoolsFragment(), false);
-                break;
-            case R.id.main_menu_saves:
-                show(new SavesFragment(), false);  // Forward animation
                 break;
             case R.id.main_menu_settings:
                 show(new SettingsFragment(), false);  // Forward animation
