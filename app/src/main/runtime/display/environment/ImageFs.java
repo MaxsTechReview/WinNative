651fe8455ada612995320da1fa95facde1c14ea9
ribbit384 <204555885+ribbit384@users.noreply.github.com>
2026-05-19 13:04:45 -0700

    Fix ImageFS version import

--- app/src/main/runtime/display/environment/ImageFs.java
+++ app/src/main/runtime/display/environment/ImageFs.java
@@ -6,6 +6,7 @@ import com.winlator.cmod.runtime.wine.WineInfo;
 import com.winlator.cmod.shared.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class ImageFs {
